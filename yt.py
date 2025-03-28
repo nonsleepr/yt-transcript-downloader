850b80accc1908b786d2a7a29795a5374e9195bb
Alex Bason <nonsleepr@gmail.com>
2025-03-28 08:36:53 -0400

    Use subtitles if available

--- yt.py
+++ yt.py
@@ -40,8 +40,11 @@ def fetch_transcript(video_url):
     chapters = metadata.get('chapters') or []
 
     # Fetch TTML transcript URL
-    automatic_captions = metadata.get('automatic_captions', {})
-    captions = automatic_captions.get('en-orig') or automatic_captions.get('en', [])
+    captions = metadata.get('subtitles', {}).get('en', [])
+    if not captions:
+        automatic_captions = metadata.get('automatic_captions', {})
+        captions = automatic_captions.get('en-orig') or automatic_captions.get('en', [])
+
     transcript_url = next((item['url'] for item in captions if item['ext'] == 'ttml'), None)
 
     if not transcript_url:
